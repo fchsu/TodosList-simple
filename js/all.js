a1e7d2ea541482a36b537fa6f63c2c69d8b1833e
Gavin <f.c.hsu0723@gmail.com>
2018-05-24 17:52:39 +0800

    updated 20180524

--- js/all.js
+++ js/all.js
@@ -1,77 +1,127 @@
 // 全域變數設定
-// Vue.component('todoList', {
-//   template: 
-//     `<ul>
-//       <input type="checkbox" v-model="newThingTodo.complete">
-//       <li :class="{checkbox: newThingTodo.complete}">{{ newThingTodo.content }}</li>
-//     </ul>`,
-//   props: {
-//     new: null
-//   },
-//   data(){
-//     return {
-//       newThingTodo: this.new
-//     }
-//   }
-// });
-let newInput = Vue.extend({  // 無法呼叫 app(Vue) 中的 methods (addTodo)
+let newInput = Vue.extend({
   template: 
-    `<div>
-      <input type="text" placeholder="請輸入代辦事項" v-model="newInputTodo"
-        @keyup.enter="addTodo(newInputTodo)">
-      <input type="button" value="送出" @click="addTodo(newInputTodo)">
+    `<div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="請輸入代辦事項"
+        aria-label="Recipient's username" aria-describedby="basic-addon2"
+        v-model="newInputTodo" @keyup.enter="addThing(newInputTodo)">
+      <button class="btn btn-outline-secondary" type="button"
+        @click="addThing(newInputTodo)">送出</button>
     </div>`,
+
   props: {
     newAdd: null
   },
   data(){
     return {
-      newInputTodo: this.newAdd
+      newInputTodo: this.newAdd,
+    }
+  },
+  methods: {
+    addThing(content){
+      const thing = {
+        content: content,
+        complete: false
+      }
+      this.$parent.$emit('updatedAdd', thing);
+      this.newInputTodo = '';
     }
   }
 });
 
 let todoList = Vue.extend({
   template: 
-    `<ul>
-      <input type="checkbox" v-model="newThingTodo.complete">
-      <li :class="{checkbox: newThingTodo.complete}">{{ newThingTodo.content }}</li>
+    `<ul class="list-group list-group-flush">
+      <li class="list-group-item" v-for="item in thingTodos">
+        <input type="checkbox" v-model="item.complete">
+        <span :class="{ checkbox: item.complete }">{{ item.content }}</span>
+        <input type="button" value="刪除" @click="removeThing(item)">
+      </li>
     </ul>`,
   props: {
-    new: null
+    todolists: null,
   },
   data(){
     return {
-      newThingTodo: this.new
+      thingTodos: this.todolists,
+    }
+  },
+  methods:{
+    removeThing(object){
+      this.$parent.$emit('updatedRemove', object);
     }
   }
 });
 
-
-let app = new Vue({
-  el:'#app',
-  data: {
-    todos: [],
-    newTodo: ''
-  },
+let wantTodo = Vue.extend({
+  template:
+  `<div class="todolist">
+    <new-input :new-add="newTodo"></new-input>
+    <todo-list :todolists="todos"></todo-list>
+  </div>`,
   components: {
     newInput,
     todoList
   },
+  mounted: function(){
+    this.$on('updatedAdd', this.addTodo);
+    this.$on('updatedRemove', this.removeTodo);
+  },
+  data(){
+    return{
+      todos: [],
+      newTodo: ''
+    }
+  },
   methods: {
-    addTodo(){
-      this.todos.push(
-        {
-          content: this.newTodo,
-          complete: false
-        }
-      );
+    addTodo(thing){
+      this.todos.push(thing);
     },
-    removeTodo(){
-
+    removeTodo(object){
+      this.todos.splice(this.todos.indexOf(object), 1);
     }
   }
 });
+
+let app = new Vue({
+  el:'#app',
+  data: {
+    todoTitle: '代辦事項',
+    doneTitle: '已完成'
+  },
+  components: {
+    wantTodo
+  }
+});
+
+
+
+
+// let app = new Vue({
+//   el:'#app',
+//   data: {
+//     todoTitle: '代辦事項',
+//     doneTitle: '已完成',
+//     todos: [],
+//     newTodo: ''
+//   },
+//   mounted: function(){
+//     this.$on('updatedAdd', this.addTodo);
+//     this.$on('updatedRemove', this.removeTodo);
+//   },
+//   components: {
+//     newInput,
+//     todoList
+//   },
+//   methods: {
+//     addTodo(thing){
+//       this.todos.push(thing);
+//     },
+//     removeTodo(object){
+//       this.todos.splice(this.todos.indexOf(object), 1);
+//     }
+//   }
+// });
 
 
 
