7bd391ee29860218de87ef7387e4ed2d7a36df83
Gavin <f.c.hsu0723@gmail.com>
2018-05-25 14:25:11 +0800

    update 20180525

--- js/all.js
+++ js/all.js
@@ -1,4 +1,13 @@
 // 全域變數設定
+let EventBus = new Vue();
+Object.defineProperties(Vue.prototype, {
+  $bus: {
+    get: function () {
+      return EventBus
+    }
+  }
+})
+
 let newInput = Vue.extend({
   template: 
     `<div class="input-group mb-3">
@@ -23,8 +32,35 @@ let newInput = Vue.extend({
         content: content,
         complete: false
       }
-      this.$parent.$emit('updatedAdd', thing);
+      this.$bus.$emit('updatedAdd', thing);
       this.newInputTodo = '';
+    }
+  }
+});
+
+let doneCancel = Vue.extend({
+  template:
+    `<input type="button" :value="state" @click="done()"></input>`,
+  props: {
+    doneThing: null,
+    stateValue: null
+  },
+  data(){
+    return {
+    doneThing: null,
+      todoState: this.doneThing,
+      state: this.stateValue
+    }
+  },
+  methods: {
+    done(){
+      if (this.state == '完成'){
+        this.$bus.$emit('updateState', true);
+        this.state = '取消';
+      }else{
+        this.$bus.$emit('updateState', false);
+        this.state = '完成';
+      }
     }
   }
 });
@@ -33,39 +69,50 @@ let todoList = Vue.extend({
   template: 
     `<ul class="list-group list-group-flush">
       <li class="list-group-item" v-for="item in thingTodos">
-        <input type="checkbox" v-model="item.complete">
         <span :class="{ checkbox: item.complete }">{{ item.content }}</span>
+        <done-cancel :doneThing="item.complete"
+            :stateValue="todoState"></done-cancel>
         <input type="button" value="刪除" @click="removeThing(item)">
       </li>
     </ul>`,
   props: {
-    todolists: null,
+    todolists: null
+  },
+  components: {
+    doneCancel
+  },
+  mounted: function(){
+    this.$bus.$on('updateState', updateData);
   },
   data(){
     return {
       thingTodos: this.todolists,
+      todoState: '完成'
     }
   },
   methods:{
     removeThing(object){
-      this.$parent.$emit('updatedRemove', object);
+      this.$bus.$emit('updatedRemove', object);
+    },
+    updateData(Boolean){
+      this;
     }
   }
 });
 
 let wantTodo = Vue.extend({
   template:
-  `<div class="todolist">
-    <new-input :new-add="newTodo"></new-input>
-    <todo-list :todolists="todos"></todo-list>
-  </div>`,
+    `<div class="todolist">
+      <new-input :new-add="newTodo"></new-input>
+      <todo-list :todolists="todos"></todo-list>
+    </div>`,
   components: {
     newInput,
     todoList
   },
   mounted: function(){
-    this.$on('updatedAdd', this.addTodo);
-    this.$on('updatedRemove', this.removeTodo);
+    this.$bus.$on('updatedAdd', this.addTodo);
+    this.$bus.$on('updatedRemove', this.removeTodo);
   },
   data(){
     return{
@@ -83,50 +130,42 @@ let wantTodo = Vue.extend({
   }
 });
 
+let doneTodo = Vue.extend({
+  template: 
+    `<div class="todolist">
+      <todo-list :todolists="todos"></todo-list>
+    </div>`,
+  components: {
+    todoList
+  },
+  data(){
+    return{
+      todos: [],
+    }
+  },
+});
+
 let app = new Vue({
   el:'#app',
   data: {
     todoTitle: '代辦事項',
-    doneTitle: '已完成'
+    doneTitle: '已完成',
+    currentView: 'wantTodo'
   },
   components: {
-    wantTodo
-  }
+    wantTodo,
+    doneTodo
+  },
+  computed: {
+    wantBorderOnOff(){
+      return{
+        titleBorder: this.currentView == 'wantTodo'
+      }   
+    },
+    doneBorderOnOff(){
+      return{
+        titleBorder: this.currentView == 'doneTodo'
+      }
+    }
+  },
 });
-
-
-
-
-// let app = new Vue({
-//   el:'#app',
-//   data: {
-//     todoTitle: '代辦事項',
-//     doneTitle: '已完成',
-//     todos: [],
-//     newTodo: ''
-//   },
-//   mounted: function(){
-//     this.$on('updatedAdd', this.addTodo);
-//     this.$on('updatedRemove', this.removeTodo);
-//   },
-//   components: {
-//     newInput,
-//     todoList
-//   },
-//   methods: {
-//     addTodo(thing){
-//       this.todos.push(thing);
-//     },
-//     removeTodo(object){
-//       this.todos.splice(this.todos.indexOf(object), 1);
-//     }
-//   }
-// });
-
-
-
-// 動作與監聽
-
-
-
-// 函式設定
